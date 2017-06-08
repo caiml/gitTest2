b152b4160851b673846af7d675955fc2ef89a11f
caiml <caimemory.gmail.com>
2017-06-08 20:44:59 +0800

    change two

--- main/main.js
+++ main/main.js
@@ -1,10 +1,10 @@
 // Write your cade below:
 module.exports = function main() {
-  var a=9;
-	var b=4;
+  
+	
     function deplex(x,y){
 		var z=x%y;
 		return z;
 	}
-	console.log(deplex(a,b));
+	console.log(deplex(9,4));
 };
