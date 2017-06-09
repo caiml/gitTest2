6b84c99a0b5ac424c78612af291041db1793f5d9
caiml <caimemory.gmail.com>
2017-06-09 10:12:01 +0800

    try twelve

--- main/main.js
+++ main/main.js
@@ -1,15 +1,16 @@
 // Write your cade below:
-module.exports = function main(x,y) {
-	//function calculate(x,y){
+module.exports = function main() {
+	function calculate(x,y){
 	this.x=x;
 	this.y=y;
 	var z=x%y;
 	return z;
-	//}
+	}
 	//console.log(calculate(9,3));
 	//console.log(calculate(11,4));
+	var z=new calculate(9,3);
+    console.log(z);
+    var a=new calculate(11,4);
+    console.log(a);
 };
-var z=new main(9,3);
-console.log(z);
-var a=new main(11,4);
-console.log(a);
+
