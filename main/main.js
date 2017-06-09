32c1bff895ec3bf2fb2dbc39bb7a90cb82ac4d64
caiml <caimemory.gmail.com>
2017-06-09 12:41:55 +0800

    try

--- main/main.js
+++ main/main.js
@@ -1,14 +1,12 @@
 // Write your cade below:
 function calculate(x,y){
-	if (y !=0){
 	var z=x%y;
 	return z;
 	}
-	else console.log("输入无效");
-	}
 	
-module.exports = function main(x,y) {
-	calculate(x,y);
-}
+	
+ 
+
+alert("ssss:"+calculate(11,4))
 	
 
