51743ffa9116417cd4826fed30c441f93e5dcc0d
caiml <caimemory.gmail.com>
2017-06-09 09:57:40 +0800

    try ten

--- main/main.js
+++ main/main.js
@@ -1,7 +1,11 @@
 // Write your cade below:
-module.exports = function main(x,y) {
+module.exports = function main() {
+	function calculate(x,y){
 	this.x=x;
 	this.y=y;
 	var z=x%y;
-	console.log(z);
+	return z;
+	}
+	console.log(calculate(9,3));
+	console.log(calculate(11,4));
 };
