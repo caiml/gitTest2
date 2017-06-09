096dd5e7b4299826b14a2a53caa66ef826038134
caiml <caimemory.gmail.com>
2017-06-09 13:30:55 +0800

    trytry

--- main/main.js
+++ main/main.js
@@ -1,12 +1,6 @@
 // Write your cade below:
-function calculate(x,y){
-	var z=x%y;
-	return z;
-	}
-	
-	
- 
-
-alert("ssss:"+calculate(11,4))
-	
+module.exports = function main(x,y) {
+		var z=x%y;
+	console.log(z);
+};
 
