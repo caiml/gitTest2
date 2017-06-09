fd2088b2bb1d09e622bc8f2ea269f9801b8c1e08
caiml <caimemory.gmail.com>
2017-06-09 10:29:16 +0800

    try thrp

--- main/main.js
+++ main/main.js
@@ -1,17 +1,14 @@
 // Write your cade below:
 module.exports = function main() {
 	function calculate(x,y){
-	this.x=x;
-	this.y=y;
 	var z=x%y;
 	return z;
 	}
 	//console.log(calculate(9,3));
 	//console.log(calculate(11,4));
-	
+	var a=calculate(9,3);
+	console.log(a);
+	var b=calculate(11,4);
+	console.log(b);	
 };
-var z=new main.calculate(9,3);
-    console.log(z);
-    var a=new main.calculate(11,4);
-    console.log(a);
 
